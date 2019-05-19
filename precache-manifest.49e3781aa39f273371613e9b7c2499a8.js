388c9a7b59dd64cd8589d0d6b8dbbdff9d02d234
kevin <kpcurtis@gmail.com>
2019-05-19 15:27:14 +0100

    Updates

---
+++ precache-manifest.49e3781aa39f273371613e9b7c2499a8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a0efedc556c215e103485b867272104",
+    "url": "/DIce_roller/index.html"
+  },
+  {
+    "revision": "4fe3309c66627a0502ec",
+    "url": "/DIce_roller/static/css/main.84278b9c.chunk.css"
+  },
+  {
+    "revision": "2790c44532533aa1f119",
+    "url": "/DIce_roller/static/js/2.2668f2aa.chunk.js"
+  },
+  {
+    "revision": "4fe3309c66627a0502ec",
+    "url": "/DIce_roller/static/js/main.8197f740.chunk.js"
+  },
+  {
+    "revision": "668bcd7d1f1bcbae8a67",
+    "url": "/DIce_roller/static/js/runtime~main.a57ca0cd.js"
+  }
+]);
